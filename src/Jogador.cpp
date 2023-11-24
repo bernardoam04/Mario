360bb4e1b7ea3f1f3b49d68596089e41a35a629a
Bernardo Alves <bernardoam@ufmg.br>
2023-11-24 13:43:58 -0300

    Metodos de deslocamento Personagens
    Correcao dos metodos de deslocamento dos personagens, criacao inicial (sem colisoes)
    de metodos para movimentar personagens genericos (nao controlados pelo usuario) e um especifico para o Jogador

--- src/Jogador.cpp
+++ src/Jogador.cpp
@@ -3,3 +3,38 @@
 Jogador::Jogador(){
     jogadorTexture.loadFromFile("../imagens/cogumelo.png");
 }
+
+void Jogador::modificarPosicao(sf::Time deltaTime) {
+    // Obtendo valores atuais
+    float velocidadeHorizontalAtual = getVelocidadeHorizontal();
+    float velocidadeVerticalAtual = getVelocidadeVertical();
+    bool estaNoArAtual = getEstaNoAr();
+    sf::Vector2f posicaoAtual = getPosicao();
+
+    // Configurando nova velocidade horizontal
+    setVelocidadeHorizontal(100.0f);
+
+    // Movimentação horizontal
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+        posicaoAtual.x -= velocidadeHorizontalAtual * deltaTime.asSeconds();
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+        posicaoAtual.x += velocidadeHorizontalAtual * deltaTime.asSeconds();
+    }
+
+    // Verificação para pulo
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !estaNoArAtual) {
+        setVelocidadeVertical(-300.0f);
+        setEstaNoAr(true);
+    }
+
+    // Aplicação da gravidade se estiver no ar
+    if (estaNoArAtual) {
+        setVelocidadeVertical(velocidadeVerticalAtual + 1); // IMPLEMENTAR GRAVIDADE
+    }
+
+    // Atualização da posição vertical
+    posicaoAtual.y += velocidadeVerticalAtual * deltaTime.asSeconds();
+
+    // Configurando a nova posição
+    setPosicao(posicaoAtual);
+}
--- src/Personagem.cpp
+++ src/Personagem.cpp
@@ -13,22 +13,50 @@ void Personagem::desenharPersonagem(sf::RenderWindow& janela)
 }
 
 void Personagem::modificarPosicao(sf::Time deltaTime) {
-    velocidadeHorizontal = 100.0f;
+    // Implementação padrão para personagens não controlados pelo jogador
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        posicaoPersonagem.x -= velocidadeHorizontal * deltaTime.asSeconds();
-    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        posicaoPersonagem.x += velocidadeHorizontal * deltaTime.asSeconds();
-    }
+    // Lógica de movimentação automática (Sera melhorada conforme a colisao ainda)
+    float velocidadeHorizontal = 50.0f;
+    float aceleracaoGravidade = 9.8f;
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && estaNoAr == false) {
-        velocidadeVertical = -300.0f;
-        estaNoAr = true;
-    }
+    // Movimentação automática para a direita
+    posicaoPersonagem.x += velocidadeHorizontal * deltaTime.asSeconds();
 
-    if (estaNoAr) {
-        velocidadeVertical += 1;//IMPLEMENTAR GRAVIDADE PARA TIRAR ESSE NUM MAGICO
-    }
-
+    // Simulação simples de gravidade
+    velocidadeVertical += aceleracaoGravidade;
     posicaoPersonagem.y += velocidadeVertical * deltaTime.asSeconds();
+
+
+}
+
+sf::Vector2f Personagem::getPosicao() const {
+    return posicaoPersonagem;
+}
+
+float Personagem::getVelocidadeHorizontal() const {
+    return velocidadeHorizontal;
+}
+
+float Personagem::getVelocidadeVertical() const {
+    return velocidadeVertical;
+}
+
+bool Personagem::getEstaNoAr() const {
+    return estaNoAr;
+}
+
+void Personagem::setPosicao(const sf::Vector2f& posicao) {
+    posicaoPersonagem = posicao;
+}
+
+void Personagem::setVelocidadeHorizontal(float velocidade) {
+    velocidadeHorizontal = velocidade;
+}
+
+void Personagem::setVelocidadeVertical(float velocidade) {
+    velocidadeVertical = velocidade;
+}
+
+void Personagem::setEstaNoAr(bool estado) {
+    estaNoAr = estado;
 }
