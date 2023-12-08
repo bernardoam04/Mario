b27bfce58d53e4ddd785933775a14481864a6b6d
Bernardo Alves <bernardoam@ufmg.br>
2023-12-07 22:52:50 -0300

    Documentacao Goomba Cometarios doxygen

--- Documentacao/html/search/all_7.js
+++ Documentacao/html/search/all_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['mapa_0',['Mapa',['../class_mapa.html',1,'']]],
   ['menu_1',['Menu',['../class_menu.html',1,'']]],
-  ['modificarposicao_2',['modificarPosicao',['../class_jogador.html#a453e4ce824fb4388742b918ca1608cee',1,'Jogador']]]
+  ['modificarposicao_2',['modificarposicao',['../class_goomba.html#a61093bd89d7d40d5f8a7802e91febf3c',1,'Goomba::modificarPosicao()'],['../class_jogador.html#a453e4ce824fb4388742b918ca1608cee',1,'Jogador::modificarPosicao()']]],
+  ['morrer_3',['morrer',['../class_goomba.html#a8a86996227e2f2c5a1d03956c15eb3de',1,'Goomba']]]
 ];
--- Documentacao/html/search/all_a.js
+++ Documentacao/html/search/all_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['setcenter_0',['setCenter',['../class_camera.html#a492690cc33179e5f18ff2cfcedf755e6',1,'Camera']]],
-  ['setmovendodireita_1',['setMovendoDireita',['../class_jogador.html#a2c47e6f2df76849c27b420a351944bba',1,'Jogador']]],
-  ['soundmanager_2',['SoundManager',['../class_sound_manager.html',1,'']]]
+  ['soundmanager_1',['SoundManager',['../class_sound_manager.html',1,'']]]
 ];
